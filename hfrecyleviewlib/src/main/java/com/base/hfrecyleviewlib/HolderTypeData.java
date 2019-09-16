f3a92a0714dc144882d24b13c6b05a719320d5cd
Sudroid <1019887259@qq.com>
2019-09-16 10:26:15 +0800

    add

---
+++ hfrecyleviewlib/src/main/java/com/base/hfrecyleviewlib/HolderTypeData.java
@@ -0,0 +1,12 @@
+package com.base.hfrecyleviewlib;
+
+import android.view.ViewGroup;
+
+public interface  HolderTypeData<VH extends BasicRecyViewHolder> {
+
+    int getType();
+
+    BasicRecyViewHolder buildHolder(ViewGroup parent);
+
+    void bindDatatoHolder(VH vh,int postion,int type);
+}
